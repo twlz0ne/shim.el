520c919967d941a5026b906be150aee818069c1c
Gong Qijian <gongqijian@gmail.com>
2018-12-25 19:23:59 +0800

    Initial version

---
+++ README.md
@@ -0,0 +1,72 @@
+[![Build Status](https://travis-ci.com/twlz0ne/shim.el.svg?branch=master)](https://travis-ci.com/twlz0ne/shim.el)
+
+## shim.el
+
+Emacs integration for Xenv.
+
+## Installation
+
+Clone this repository, or install from MELPA. Add the following to your `.emacs`:
+
+```elisp
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/shim.el"))
+(require 'shim)
+(shim-init-java)
+(shim-init-node)
+(shim-init-python)
+(shim-init-ruby)
+```
+
+## Usage
+
+Manually:
+
+- `M-x shim-set` input version or select from list.
+- `M-x shim-auto-set` auto detect version.
+
+Or add to major mode hook:
+
+```elisp
+(add-hook 'js-mode-hook #'shim-mode)
+```
+
+If you would like specific version by file local variable:
+
+```javascript
+...
+// Local Variables:
+// shim-node-version: "8.11.3"
+// End:
+```
+
+Then you have to add following instead:
+
+```elisp
+(add-hook 'hack-local-variables-hook
+          (lambda ()
+            (when (ignore-errors (shim--guess-language))
+              (shim-mode 1))))
+```
+
+Register major mode:
+
+```elisp
+(shim-register-mode 'node 'js2-mode)
+
+;; or registers all modes at initialization
+(shim-init-node :major-modes
+                '(js-mode
+                  js2-mode
+                  rjsx-mode))
+```
+
+Add support for new language
+
+```elisp
+(cl-defun shim-init-foo (&key (major-modes '(foo-mode)) (executable "fooenv"))
+  (shim-init
+   (make-shim--shim
+    :language 'foo
+    :major-modes major-modes
+    :executable executable)))
+```
---
+++ test-nodeshim.el
@@ -0,0 +1,132 @@
+;;; test-nodeshim.el --- Test nodeshim -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018 Gong QiJian <gongqijian@gmail.com>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(require 'ert)
+(require 'shim)
+
+(shim-init-node :major-modes '())
+(shim-register-mode 'node 'js-mode)
+(assert (boundp 'shim-node-version))
+(assert (equal '(js-mode) (shim--shim-major-modes (cdr (assq 'node shim--shims)))))
+
+(defun test-nodeshim--make-project (specs)
+  "Make empty project and create directory & files specified by SPECS.
+Example of SPECS:
+
+        '(\"src/\"
+          (\"src/.node-version\" . \"6.0.0\"))
+
+Return project root."
+  (let ((root (make-temp-file "test-nodeshim--" 'root "/")))
+    (dolist (spec specs)
+      (cond
+       ((consp spec)
+        (let ((file-path (concat root (car spec)))
+              (file-content (cdr spec)))
+          (make-directory (file-name-directory file-path) t)
+          (with-temp-buffer
+            (insert file-content)
+            (write-region (point-min) (point-max) file-path))))
+       (t
+        (let ((folder-path (concat root spec)))
+          (make-directory (file-name-directory folder-path) t)))))
+    root))
+
+(defun test-nodeshim--newest-version ()
+  "Get newest version of node."
+  (car (reverse (shim-versions 'node))))
+
+(defun test-nodeshim--open-file (file-name &optional defer-p)
+  "Open file `FILE-NAME', return node version if `DEFER-P' is nil (the default)."
+  (setq enable-local-variables :all)
+  (find-file file-name)
+  (js-mode)
+  (unless defer-p
+    (shim-auto-set)
+    (getenv "NODENV_VERSION")))
+
+(ert-deftest test-nodeshim-node-version-file-0 ()
+  (let ((root (test-nodeshim--make-project '(("test.js" . "\n")))))
+    (should (equal (test-nodeshim--newest-version)
+                   (test-nodeshim--open-file (concat root "test.js"))))))
+
+(ert-deftest test-nodeshim-node-version-file-1 ()
+  (let ((root (test-nodeshim--make-project '(("test.js" . "\n")
+                                           (".node-version" . "6.0.0")))))
+    (should (equal "6.0.0"
+                   (test-nodeshim--open-file (concat root "test.js"))))))
+
+(ert-deftest test-nodeshim-node-version-file-2 ()
+  (let ((root (test-nodeshim--make-project '(("test1.js" . "\n")
+                                           (".node-version" . "6.0.0")
+                                           ("src/test2.js" . "\n")
+                                           ("src/.node-version" . "7.0.0")))))
+    (should (equal "6.0.0" (test-nodeshim--open-file (concat root "test1.js"))))
+    (should (equal "7.0.0" (test-nodeshim--open-file (concat root "src/test2.js"))))))
+
+(ert-deftest test-nodeshim-node-version-file-3 ()
+  (let ((root (test-nodeshim--make-project '(("foo/test1.js" . "\n")
+                                           ("foo/.node-version" . "6.0.0")
+                                           ("bar/test2.js" . "\n")
+                                           ("bar/.node-version" . "7.0.0")))))
+    (should (equal "6.0.0" (test-nodeshim--open-file (concat root "foo/test1.js"))))
+    (should (equal "7.0.0" (test-nodeshim--open-file (concat root "bar/test2.js"))))))
+
+(ert-deftest test-nodeshim-local-version ()
+  (let ((root (test-nodeshim--make-project
+               (list (cons ".node-version" "6.0.0")
+                     (cons "test-without-local-variable.js" "\n")
+                     (cons "test-with-local-variable.js" (concat
+                                                          "// Local Variables:\n"
+                                                          "// shim-node-version: \"7.0.0\"\n"
+                                                          "// End:\n"))))))
+    ;; Use node version specified in .node-version
+    (should (equal "6.0.0" (test-nodeshim--open-file (concat root "test-without-local-variable.js"))))
+
+    ;; Use node version specified by local variable
+    (add-hook 'hack-local-variables-hook
+              (lambda ()
+                (when shim-node-version
+                  (shim-auto-set)
+                  (should (equal "7.0.0" (shim-version))))))
+    (test-nodeshim--open-file (concat root "test-with-local-variable.js") t)
+    ))
+
+(defun test-nodeshim--read-version-from-file (file)
+  "Read version from FILE."
+  (when (file-exists-p file)
+    (replace-regexp-in-string
+     "\\(?:\n\\)\\'" "" (shell-command-to-string (format "head -n 1 %s" file)))))
+
+(ert-deftest test-nodeshim-get-version-in-temp-buffer ()
+  (let ((global-ver (or (test-nodeshim--read-version-from-file "~/.node-version")
+                        (test-nodeshim--newest-version))))
+    (should
+     (equal
+      global-ver
+      (with-temp-buffer
+        (js-mode)
+        (shim-auto-set)
+        (shim-version)
+        )))
+    ))
+
+(provide 'test-nodeshim)
+
+;;; test-nodeshim.el ends here
